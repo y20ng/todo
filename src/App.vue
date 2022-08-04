7c756ec280d1ca9a39d9b3dd0890eb421fb9dc78
y20ng <87141522+y20ng@users.noreply.github.com>
2022-08-04 17:38:23 +0900

    Made Todo
    Todo list using vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input
+      v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    />
+    <hr />
+    <Todo
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from "@/components/Todo.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Todo,
+  },
+  data() {
+    return {
+      todoText: "",
+      todos: [
+        { id: 1, text: "buy a car", checked: false },
+        { id: 2, text: "play game", checked: false },
+      ],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      const index = this.todos.findIndex((todo) => {
+        return todo.id === id;
+      });
+      this.todos.splice(index, 1);
+      // this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = "";
+    },
+    toggleCheckbox({ id, checked }) {
+      const index = this.todos.findIndex((todo) => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
